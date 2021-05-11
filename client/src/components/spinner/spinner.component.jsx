a03e57b2ab5c1df6eac3ab072434b4b976fa711f
MatheusTrinca <matheusfelipetrinca@gmail.com>
2021-05-11 17:15:38 -0300

    pwa files

--- client/src/components/spinner/spinner.component.jsx
+++ client/src/components/spinner/spinner.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { SpinnerContainer, SpinnerOverlay } from './spinner.styles';
 
 const Spinner = () => (
-  <SpinnerContainer>
-    <SpinnerOverlay />
-  </SpinnerContainer>
+  <SpinnerOverlay>
+    <SpinnerContainer />
+  </SpinnerOverlay>
 );
 
 export default Spinner;
